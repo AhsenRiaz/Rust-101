0c2485923eacd2ad90b293e13ad6b56e6e7e5749
ahsenriaz <ahsenriaz2001@gmail.com>
2023-03-02 17:48:49 +0500

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("firstName: {} and lastName: {}", first_name(), last_name());
+}
+
+fn first_name() -> String {
+    let first_name = String::from("Muhammad Ahsen");
+    first_name
+}
+
+fn last_name() -> String {
+    let second_name = String::from("Riaz");
+    second_name
+}
