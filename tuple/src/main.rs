669b9225f0dbbf2cec258af4c0cca910aaa9f38a
AhsenRiaz <ahsenriaz2001@gmail.com>
2023-03-19 16:41:56 +0500

    tuple

---
+++ tuple/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let coord = (2, 3);
+    println!("{}, {}", coord.0, coord.1);
+
+    let (x, y) = (10, 20);
+    println!("{}, {}", x, y);
+
+    let user_info = ("Bob", 21);
+    println!("{}, {}", user_info.0, user_info.1);
+}
